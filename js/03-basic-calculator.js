69a3fd223b20dd3388dfcfddfae71a011caa36f3
Ranjitha Kn <ranju.kn@gmail.com>
2023-03-09 14:40:41 -0800

    Update2

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,15 +1,42 @@
 // IMPORT THE MODULE
-import { add,sub,mul,div } from "./calculator.js";
+import { add,sub,mul,div } from './calculator.js';
 // COLLECT FIRST NUMBER FROM USER
-document.write("HI");
+/*
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+// SUBTRACT FUNCTION
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+// MULTIPLY FUNCTION
+function mul(num1, num2) {
+    return num1 * num2;
+}
+
+// DIVIDE FUNCTION
+function div(num1, num2) {
+    if (num1 == 0) {
+        document.write("Divide by zero error");
+    }
+    else
+        return num1 / num2;
+}
+*/
+document.write("SIMPLE CALCULATOR");
+
+let a, b, c, d;
+let choice = 'y';
+// CHECK TO SEE WHAT OPERATION THEY'RE USING
+do {
 let first_num = parseFloat(prompt("Enter first number"));
 // COLLECT SECOND NUMBER FROM USER
 let second_num = parseFloat(prompt("Enter second number"));
 // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
 let ch = prompt("Enter operator +,-,*,/");
-let a, b, c, d;
-// CHECK TO SEE WHAT OPERATION THEY'RE USING
-do {
+
     switch (ch) {
         case '+': a = add(first_num, second_num);
             alert(first_num + ch + second_num + "=" + a);
@@ -25,5 +52,6 @@ do {
             break;
         default: document.write("Invalid choice");
     }
-} while (1);
+    choice = prompt("Do you want to continue, y/n");
+} while (choice=='y');
 // CALL THE APPROPRIATE FUNCTION
